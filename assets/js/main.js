7ae2c123d4b49f91b1b5e24debe3fa829734994e
allanahmurphy <allanahtmurphy@gmail.com>
2020-08-15 20:34:14 +0000

    Styling of index page showcards

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(".showcard").mouseover(function(){
+  $(this).css({"background-color": "rgba(26,166,183,0.25)", "border": "10px solid rgba(26,166,183,0)"});
+});
+
+$(".showcard").mouseleave(function(){
+  $(this).css({"background-color": "#EBECF0", "border": "10px solid white",});
+});
