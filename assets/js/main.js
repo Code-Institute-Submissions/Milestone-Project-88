e2a405b2c126908c3bf7918674aa838297aa25ff
allanahmurphy <allanahtmurphy@gmail.com>
2020-08-16 12:01:11 +0000

    Showcard jquery fixed and working, css updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
 $(".showcard").mouseover(function(){
-  $(this).css({"background-color": "rgba(26,166,183,0.25)", "border": "10px solid rgba(26,166,183,0)"});
+  $(this).css({"background-color": "rgba(26,166,183,0.25)"});
 });
 
 $(".showcard").mouseleave(function(){
-  $(this).css({"background-color": "#EBECF0", "border": "10px solid white",});
+  $(this).css({"background-color": "#EBECF0"});
 });
+
+//Code Adapted from https://www.youtube.com/watch?v=uI18xGocVnw
+$(".readmore-btn").on('click', function (){
+    $(this).parent().toggleClass("showContent");
+    //Shorthand if Statement
+    var replaceText = $(this).parent().hasClass("showContent") ? "Read Less ▲" : "Read More ▼"
+    $(this).text(replaceText);
+
+})
