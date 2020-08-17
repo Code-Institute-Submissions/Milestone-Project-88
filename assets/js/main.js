e08919d470b087737b8a4364018419919d68b8da
allanahmurphy <allanahtmurphy@gmail.com>
2020-08-17 20:46:07 +0000

    Interactive Map working with dropdown menu for restaurants and hotels

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,8 +53,10 @@ function drawWeather( d ) {
 //Trip Advisor API Connection via RapidAPI
 
 //Based on RapidAPI documentation for Trip Advisor https://rapidapi.com/apidojo/api/tripadvisor1?endpoint=apiendpoint_c0d6decf-e541-447d-bc87-2fa023cd96d6
-function restaurantList (LocationID) {
-    fetch("https://tripadvisor1.p.rapidapi.com/restaurants/list?restaurant_tagcategory_standalone=10591&lunit=km&restaurant_tagcategory=10591&limit=30&currency=USD&lang=en_US&location_id=" + LocationID, {
+
+//Restaurant Locations
+function restaurantList (lat, lon) {
+    fetch('https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng?limit=30&currency=USD&distance=30&lunit=km&lang=en_US&latitude=' + lat + '&longitude=' + lon, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
@@ -65,51 +67,84 @@ function restaurantList (LocationID) {
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
         console.log(data);
-        mapRestaurants (data);
+        var restaurantIcon = "http://maps.google.com/mapfiles/kml/pal2/icon35.png"
+        mapMarkers (data, restaurantIcon);
 	})
     .catch(err => {
 	console.log(err);
     });
 }
 
-var restaurantMarkers = [];
+//Hotel Locations
+function hotelList (lat, lon) {
+    fetch('https://tripadvisor1.p.rapidapi.com/hotels/list-by-latlng?lang=en_US&limit=30&currency=USD&&distance=30&latitude=' + lat + '&longitude=' + lon, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
+		"x-rapidapi-key": "2ed47672a1mshfe827335d43a73ep158491jsnc62f265ff059"
+	}
+    })
 
-function mapRestaurants (d) {  
-    
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+        console.log(data);
+        var hotelIcon = "http://maps.google.com/mapfiles/kml/pal2/icon20.png"
+        mapMarkers (data, hotelIcon);
+	})
+    .catch(err => {
+	console.log(err);
+    });
+}
+
+//Create Array of Markers with Data
+function mapMarkers (d, Icon) {  
+    var Markers = [];
     var i;
     for (var i=0; i<d.data.length; i++) {
         if ((typeof d.data[i].name !== "undefined")&&(typeof d.data[i].latitude !== "undefined")){          
-            restaurantMarkers.push({
+            Markers.push({
             name: d.data[i].name,
             lat: d.data[i].latitude,
             long: d.data[i].longitude,
             stars: d.data[i].rating,
             description: d.data[i].description,
-            urlRestaurant: d.data[i].web_url,
+            url: d.data[i].web_url,
             })  
         }
     }
-    console.log(restaurantMarkers)
-    initMap(restaurantMarkers);
-    fitBoundsToVisibleMarkers(restaurantMarkers);
+    console.log(Markers)
+    initMap(Markers, Icon);
 }
 
-function initMap(markers) {
+
+//Create Map and Add Trip Advisor Markers
+function initMap(markers, Icon) {
     var mapOptions = {
         center: new google.maps.LatLng(markers[0].lat, markers[0].long),
         zoom: 10,
         mapTypeId: 'satellite'
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    //Adjust Map Extent
+    var bounds = new google.maps.LatLngBounds();
+
+    for (var i=0; i<markers.length; i++) {
+            var latlng = new google.maps.LatLng(markers[i].lat, markers[i].long);
+            bounds.extend(latlng);   
+        }
+    
+    map.fitBounds(bounds);
     
     //Create and open InfoWindow.
     var infoWindow = new google.maps.InfoWindow();
  
-    for (var i = 0; i < restaurantMarkers.length; i++) {
-        var data = restaurantMarkers[i];
+    for (var i = 0; i < markers.length; i++) {
+        var data = markers[i];
         var myLatlng = new google.maps.LatLng(data.lat, data.long);
         var marker = new google.maps.Marker({
             position: myLatlng,
+            icon: Icon,
             map: map,
             title: data.name
         });
@@ -118,35 +153,14 @@ function initMap(markers) {
         (function (marker, data) {
             google.maps.event.addListener(marker, "click", function (e) {
                 //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
-                infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+                infoWindow.setContent(`<div style = 'width:150px;min-height:40px;font-weight:bold;text-align:left;'>Name: ` + data.name + `<br>Rating: ` + data.stars + `<br><a href="`+ data.url +`" target="_blank">Website</a></div>`);
                 infoWindow.open(map, marker);
             });
         })(marker, data);
     }
 }
 
-function fitBoundsToVisibleMarkers(markers) {
 
-    var bounds = new google.maps.LatLngBounds();
-
-    for (var i=0; i<markers.length; i++) {
-            var latlng = new google.maps.LatLng(markers[i].lat, markers[i].long);
-            bounds.extend(latlng);
-            console.log(bounds);    
-        }
-    
-    google.maps.Map.fitBounds(bounds);
-    
-
-}
-
-//window.onload = function() {
-//  this.restaurantList(670171);
-//}
-
-//window.onload = function () {
-//        initMap();
-//    }
 
 
 
