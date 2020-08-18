941ddbd6c40ea8cf8b49cb42b30e8a95445cb763
allanahmurphy <allanahtmurphy@gmail.com>
2020-08-18 08:45:30 +0000

    Ushuaia Page content added

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,7 +56,7 @@ function drawWeather( d ) {
 
 //Restaurant Locations
 function restaurantList (lat, lon) {
-    fetch('https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng?limit=30&currency=USD&distance=15&lunit=km&lang=en_US&latitude=' + lat + '&longitude=' + lon, {
+    fetch('https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng?limit=50&currency=USD&distance=15&lunit=km&lang=en_US&latitude=' + lat + '&longitude=' + lon, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
