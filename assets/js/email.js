a62c422885c9b1f827cd8417df26b55491d450a0
allanahmurphy <allanahtmurphy@gmail.com>
2020-08-18 17:54:18 +0000

    Contact email form linked to Email JS and tested

---
+++ assets/js/email.js
@@ -0,0 +1,17 @@
+function sendMail(contactform) {
+    emailjs.send("gmail", "patagonianexperience", {
+        "first_name": contactform.firstname.value,
+        "last_name": contactform.lastname.value,
+        "from_email": contactform.emailaddress.value,
+        "travel_query": contactform.query.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
